bd85528568a7ca58e991b7d68450ff1d7b0eaa58
dandles <dandles.banjarnahor@gmail.com>
2019-11-05 10:01:56 +0700

    Create Versi1.java

---
+++ Versi1.java
@@ -0,0 +1,13 @@
+package GithubProject;
+
+public class Versi1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" Versi 1 ");
+		System.out.println(" Dandles Banjarnahor ");
+
+	}
+
+}
