aad20ad07f67ac6cc934920721b7b801d3a1c26f
dandles <dandles.banjarnahor@gmail.com>
2019-11-05 11:50:56 +0700

    Revert "Revert "Update Versi1.java""
    This reverts commit ad988ebba3fa96382ba2705afb3ef0a1f079c003.

--- Versi1.java
+++ Versi1.java
@@ -5,9 +5,9 @@ public class Versi1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(" Versi 6 ");
-		System.out.println(" Dandles Banjarnahor test 6 ");
-		System.out.println(" tambah donks ");
+		System.out.println(" Versi 7 ");
+		System.out.println(" Dandles Banjarnahor test 7	 ");
+		System.out.println(" Versi 7 ");
 	}
 
 }
