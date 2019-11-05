2c8e7753fdbc2bba4e7c50c390caa23d9fc9ca0d
dandles <dandles.banjarnahor@gmail.com>
2019-11-05 11:43:23 +0700

    Versi 1.2

---
+++ Versi1.java
@@ -0,0 +1,13 @@
+package GithubProject;
+
+public class Versi1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" Versi 5 ");
+		System.out.println(" Dandles Banjarnahor test 5 ");
+		System.out.println(" tambah donks ");
+	}
+
+}
