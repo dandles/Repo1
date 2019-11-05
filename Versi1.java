8e21661299efae6cf6942cd4f6e7f1e66a6b3021
dandles <dandles.banjarnahor@gmail.com>
2019-11-05 11:51:50 +0700

    Versi 1.4

--- Versi1.java
+++ Versi1.java
@@ -5,9 +5,9 @@ public class Versi1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(" Versi 7 ");
-		System.out.println(" Dandles Banjarnahor test 7	 ");
-		System.out.println(" Versi 7 ");
+		System.out.println(" Versi 1.4 ");
+		System.out.println(" Dandles Banjarnahor test 1.4	 ");
+		System.out.println(" Versi 1.4 ");
 	}
 
 }
