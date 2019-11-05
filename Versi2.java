60743003d14c445bfaa3384afa091c4dd8ebc770
dandles <dandles.banjarnahor@gmail.com>
2019-11-05 10:16:20 +0700

    Create Versi2.java

---
+++ Versi2.java
@@ -0,0 +1,13 @@
+package GithubProject;
+
+public class Versi2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" Versi 2 ");
+		System.out.println(" Dandles Banjarnahor ");
+
+	}
+
+}
